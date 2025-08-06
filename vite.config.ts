e45de91c656ae3e35fd703dd5fddc5537771419c
0xDanielimad <72716286+danielimad@users.noreply.github.com>
2025-08-06 17:00:19 +0400

    create vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
-// Vite configuration
 export default defineConfig({
-  plugins: [vue()], // If you're using React, use `@vitejs/plugin-react`
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src') // Alias for easy imports
-    }
-  },
+  root: '.',
   build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html') // Main entry file
-    }
+    outDir: 'dist',
   },
   server: {
-    open: true // Automatically open the app in the browser
+    port: 3000,
+    open: true,
   }
 });
